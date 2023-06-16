81545c5de9f3265ffe4c310c1486f459475e6ddf
Christopher J <chrisbjones1987@yahoo.com>
2023-06-16 14:17:35 -0500

    Updated Card Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,29 @@ import "./Card.css";
 
 export default function Card() {
     return (
-        <div className="card">
-            <img src={require ("../images/zaferes.jpeg")} className="card--image"></img>
-            <div className="card--stats">
-                <img src={require ("../images/star.png")} className="card--star"></img>
-                <span>5.0</span>
-                <span className="gray">(6) ● </span>
-                <span className="gray"> USA</span>
+        <div className="cardContainer">
+            <div className="card">
+                <img src={require ("../images/swimming.jpg")} className="card--image"></img>
+                <div className="card--stats">
+                    <img src={require ("../images/star.png")} className="card--star"></img>
+                    <span>5.0</span>
+                    <span className="gray">(6) ● </span>
+                    <span className="gray"> USA</span>
+                </div>
+                <p>Diving with Oceanic Travels</p>
+                <p><span className="bold">From $136</span> / person</p>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <div className="card">
+                <img src={require ("../images/horsebackRiding.jpg")} className="card--image"></img>
+                <div className="card--stats">
+                    <img src={require ("../images/star.png")} className="card--star"></img>
+                    <span>5.0</span>
+                    <span className="gray">(6) ● </span>
+                    <span className="gray"> USA</span>
+                </div>
+                <p>Horseback Riding with Lucky Ranch</p>
+                <p><span className="bold">From $155</span> / person</p>
+            </div>
         </div>
     )
 }
